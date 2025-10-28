e1c0c17e5cb96c274895105374a1778e243fccbc
bigbizze <hpcngmoh@gmail.com>
2025-10-28 05:18:57 -0400

    pub error

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub mod fpoly;
 pub mod poly;
 pub mod tip5;
 
-mod math_error;
+pub mod math_error;
 
 pub use belt::{based_check, Belt, FieldError, PRIME, PRIME_128};
 pub use math_error::{MathError, MathResult};
